65a9c2bc1df148b90422e43ea6874755b9f360aa
Jayden R <jaydensouterrobertson@gmail.com>
2026-03-24 19:40:02 +0000

    LessonController.php loads and parse lessons JSON data - JR

--- app/controllers/LessonController.php
+++ app/controllers/LessonController.php
@@ -1,1 +1,18 @@
 <?php
+
+// Get he JSON from the lesson data file.
+$json = file_get_contents(__DIR__ . '/../data/lessons.json');
+
+// If the file couldn't be read, dies and show an error message.
+if (!$json)
+{
+    die("Could not open json file");
+}
+
+// Decode the JSON string into a PHP array.
+$lessonData = json_decode($json, true);
+
+
+// Extract the lesson title and cards into separate variables.
+$title = $lessonData['title'];
+$cards = $lessonData['cards'];
