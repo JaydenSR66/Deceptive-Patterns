c2405b5dec015817bd57da808b1533138d860919
Jayden R <jaydensouterrobertson@gmail.com>
2026-03-25 21:30:31 +0000

    Lesson variables available for frontend to render - JR

--- public/lesson.php
+++ public/lesson.php
@@ -1,3 +1,11 @@
 <?php
+// Initialise variables before loading the controller to avoid undefined variable warnings.
+$title = "";
+$cards= [];
 
+// Load the lesson controller which populates $title and $cards.
+require "../app/controllers/LessonController.php";
 ?>
+
+<script src=/js/bootstrap.bundle.min.js></script>
+<script src="/js/lesson.card.js"></script>
