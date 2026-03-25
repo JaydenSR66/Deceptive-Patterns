9796e2f5ccaeb8343b2d5624dd08a940fcb56b59
Jayden R <jaydensouterrobertson@gmail.com>
2026-03-25 21:11:43 +0000

    QuizController.php loads and parse quiz JSON data - JR

--- app/controllers/QuizController.php
+++ app/controllers/QuizController.php
@@ -1,1 +1,20 @@
 <?php
+
+// Get the JSON from the quiz data file.
+$json = file_get_contents(__DIR__ . '/../data/quiz.json');
+
+// If the file couldn't be read, dies and show an error message.
+if (!$json)
+{
+    die("Could not open json file");
+}
+
+// Decode the JSON string into an array.
+$quizData = json_decode($json, true);
+
+
+// Extract the quiz data into separate variables.
+$title = $quizData['title'];
+$description = $quizData['description'];
+$questions = $quizData['questions'];
+$explanation = $quizData['explanation'];
