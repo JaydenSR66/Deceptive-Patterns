5e89ee977afa748dd656816613f7c405e3173847
Jayden R <jaydensouterrobertson@gmail.com>
2026-03-26 17:26:38 +0000

    Quiz variables available for frontend to render - JR

--- public/quiz.php
+++ public/quiz.php
@@ -1,1 +1,11 @@
 <?php
+// Initialise variables before loading the controller to avoid undefined variable warnings.
+$title = "";
+$questions = [];
+
+// Load the quiz controller which populates $title and $questions.
+require "../app/controllers/QuizController.php";
+?>
+
+<script src="/js/bootstrap.bundle.min.js"></script>
+<script src="/js/quiz.js"></script>
